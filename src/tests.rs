8e62a765f9b35a9817eb756fa305b6d6c06aabee
Julian Ganz <neither@nut.email>
2021-06-19 21:45:23 +0200

    tests: add counting tests for all modes of iteration
    These tests verify that we at least yield as many items as there are
    nodes in the input structure.

--- src/tests.rs
+++ src/tests.rs
@@ -10,6 +10,24 @@ fn smoke(node: Node) {
     node.trans_iter().for_each(|_| ())
 }
 
+#[quickcheck]
+fn node_count_breadth_first(node: Node) -> bool {
+    let count = node.count();
+    node.trans_iter().breadth_first().count() == count
+}
+
+#[quickcheck]
+fn node_count_depth_first(node: Node) -> bool {
+    let count = node.count();
+    node.trans_iter().depth_first().count() == count
+}
+
+#[quickcheck]
+fn node_count_depth_first_unordered(node: Node) -> bool {
+    let count = node.count();
+    node.trans_iter().depth_first_unordered().count() == count
+}
+
 
 /// Dumb recursive structure for testing
 #[derive(Clone, Debug)]
