eb289d01806c0bde139bd07ebf4a90d409510d72
Julian Ganz <neither@nut.email>
2021-06-19 18:12:27 +0200

    TransIter: introduce initial version of the core Iterator type
    As its name suggests, The type introduced provides the crate's basic
    functionality. It's logic _will_ get expanded a bit, and we also plan to
    introduce a convenience trait or two. But this is it, basically. Just
    like a proper micro-crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,38 @@
+//! Transitive iterator
+
+
+/// Transitive iterator
+///
+/// The iterator yields all elements which are transitively reachable from an
+/// initial set of items through a given recursion function, including those
+/// initial items. Items discovered through a call to the recursion function
+/// will only be yielded after the item passed in that call. I.e. if the
+/// recursion function yields the "children" of a node, a node will only be
+/// yielded after its "parent".
+pub struct TransIter<F: FnMut(&T) -> I, I: IntoIterator<Item = T>, T> {
+    get_next: F,
+    queue: std::collections::VecDeque<T>,
+}
+
+impl<F: FnMut(&T) -> I, I: IntoIterator<Item = T>, T> TransIter<F, I, T> {
+    /// Create a new transitive iterator
+    ///
+    /// The iterator will yield all elements which are transitively reachable
+    /// from the `initial` item through the given `recursion` function,
+    /// including the `initial` itself.
+    pub fn new(initial: T, recursion: F) -> Self {
+        Self {get_next: recursion, queue: std::iter::once(initial).collect()}
+    }
+}
+
+impl<F: FnMut(&T) -> I, I: IntoIterator<Item = T>, T> Iterator for TransIter<F, I, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        let res = self.queue.pop_front();
+        res.as_ref().map(&mut self.get_next).map(|items| self.queue.extend(items));
+
+        res
+    }
+}
 
