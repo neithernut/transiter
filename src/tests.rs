d89e122993405cae6c35b3d0e5828745b7503498
Julian Ganz <neither@nut.email>
2021-06-19 21:20:49 +0200

    tests: add recursive structure for testing
    In addition to an impl for `Arbitrary`, we include an id, which we may
    use in tests for identificaiton of a node, and a recursive function for
    counting the nodes.

--- src/tests.rs
+++ src/tests.rs
@@ -1,2 +1,34 @@
 //! Tests
 
+use quickcheck::{Arbitrary, Gen};
+
+
+/// Dumb recursive structure for testing
+#[derive(Clone, Debug)]
+struct Node {
+    id: u128,
+    children: Vec<Self>,
+}
+
+impl Node {
+    /// Retrieve the number of nodes
+    pub fn count(&self) -> usize {
+        self.children.iter().map(Self::count).sum::<usize>() + 1
+    }
+}
+
+impl Arbitrary for Node {
+    fn arbitrary(g: &mut Gen) -> Self {
+        let children = if g.size() > 0 {
+            Arbitrary::arbitrary(&mut Gen::new(g.size() / 2))
+        } else {
+            Default::default()
+        };
+        Self {id: Arbitrary::arbitrary(g), children}
+    }
+
+    fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+        Box::new(self.children.clone().into_iter())
+    }
+}
+
