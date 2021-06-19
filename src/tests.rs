0f36e8fd68ad18c2133b86d703791720ad6593aa
Julian Ganz <neither@nut.email>
2021-06-19 23:27:58 +0200

    tests: verify breadth first item ordering
    We advertise some properties regarding the order items are yielded in
    the breadth-first mode. This test checks whether those properties hold.
    
    Unfortunately, the recursive matching involved has quadratic complexity
    w.r.t. the number of nodes, even though we try to reduce the search
    space.

--- src/tests.rs
+++ src/tests.rs
@@ -28,6 +28,40 @@ fn node_count_depth_first_unordered(node: Node) -> bool {
     node.trans_iter().depth_first_unordered().count() == count
 }
 
+#[quickcheck]
+fn node_order_breadth_first(node: Node) -> bool {
+    /// Match the ids against a sequence of (child) nodes, return the ids which
+    /// may contain the next "sibling" sequences
+    fn match_ids<'a>(ids: &'a [u128], reference: &[Node]) -> Option<&'a [u128]> {
+        if reference.len() > 0 {
+            // The generated sequence must contain a the children as a
+            // contiguous sequence.
+            ids.windows(reference.len())
+                .position(|ids| ids.iter().zip(reference).all(|(id, node)| *id == node.id))
+                .and_then(|pos| {
+                    // The same must hold for grandchildren. We also reduce the
+                    // search space based on the following:
+                    //  * Those sequences appear only after their respecive
+                    //    parents, i.e. after the "current" sequence.
+                    //  * Those sequences appear in the same order as their
+                    //    respective parents.
+                    reference
+                        .iter()
+                        .try_fold(
+                            ids.split_at(pos + reference.len()).1,
+                            |ids, sub| match_ids(ids, sub.children.as_ref())
+                        )
+                        .map(|_| ids)
+                })
+        } else {
+            Some(ids)
+        }
+    }
+
+    let ids: Vec<_> = node.clone().trans_iter().breadth_first().map(|n| n.id).collect();
+    match_ids(ids.as_ref(), &[node]).is_some()
+}
+
 
 /// Dumb recursive structure for testing
 #[derive(Clone, Debug)]
